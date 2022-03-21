e52d283d58ce59fe92aa908a4fbcf15858232a80
jnsltk <janos.litkei@gmail.com>
2022-03-21 21:43:32 +0800

    Create game() function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//Program prompts player
-let playerSelection = prompt('Please make your selection of either rock, paper or scissors: ', 'rock/paper/scissors');
+
 //Computer makes its own selection
     //function computerPlay returns a random selection of either rock paper or scissors
 function computerPlay() {
@@ -7,14 +6,15 @@ function computerPlay() {
     return computerSelection[Math.floor(Math.random() * computerSelection.length)];
 }
 
-//Compares the two selections
+//Compares the two selections and declares a winner
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    const loseMessage = `You lose! ${computerSelection} beats ${playerSelection}!`
-    const winMessage = `You win! ${playerSelection} beats ${computerSelection}!`
+    const draw =  ['noWinner', `It\'s a draw! You and the computer both played ${playerSelection}!`];
+    const loseMessage = ['computerWon', `You lose! ${computerSelection} beats ${playerSelection}!`];
+    const winMessage = ['playerWon', `You win! ${playerSelection} beats ${computerSelection}!`];
     switch (playerSelection) {
         case computerSelection: //if both selections are the same return a message that it's a draw
-            return `It\'s a draw! You and the computer both played ${playerSelection}!`;
+            return draw;
         case 'rock': //check all cases where player selects rock
             switch (computerSelection) {
                 case 'paper': 
@@ -37,11 +37,41 @@ function playRound(playerSelection, computerSelection) {
                     return winMessage;
             }
         default:
-            return 'Something went wrong!'
+            return 'Something went wrong! Did you type your answer correctly?'
     }
 }
-//Declares winner
 
-//console.log(computerPlay());
-//console.log(playerSelection);
-console.log(playRound(playerSelection, computerPlay()));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) { //game keeps going for 5 rounds
+        let playerSelection = prompt('Please make your selection of either rock, paper or scissors: ', 'rock/paper/scissors'); //game prompts player
+        let roundResult = playRound(playerSelection, computerPlay()); //calls the playRound() function, which plays a round of the game and stores who won, and the message in the array roundResult
+        switch (roundResult[0]) { //calls playRound() function to play a round of the game, and checks the result of the game to see who won
+            case 'noWinner':
+                console.log(roundResult[1] + ` Your current score: ${playerScore}, the computer's current score: ${computerScore}`);
+                break;
+            case 'computerWon':
+                computerScore++;
+                console.log(roundResult[1] + ` Your current score: ${playerScore}, the computer's current score: ${computerScore}`);
+                break;
+            case 'playerWon':
+                playerScore++;
+                console.log(roundResult[1] + ` Your current score: ${playerScore}, the computer's current score: ${computerScore}`);
+                break;
+            default: 
+                return 'Error!!!'
+                break;
+        } 
+        
+    }
+    if (playerScore > computerScore) {
+        return 'Game Over! You win!';
+    } else if (playerScore < computerScore) {
+        return 'Game Over! You lose, better luck next time!';
+    } else {
+        return 'Game Over! It\'s a draw!'
+    }
+    
+}
+console.log(game());
